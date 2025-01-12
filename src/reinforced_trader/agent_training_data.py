57ca69b52bc94c36527dfceda145885078771091
tenJnd <tomas.jnd@gmail.com>
2025-01-12 01:13:04 +0100

    fix

--- src/reinforced_trader/agent_training_data.py
+++ src/reinforced_trader/agent_training_data.py
@@ -1,59 +1,59 @@
-from datetime import datetime
-
 import joblib
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-from src.utils.utils import (calculate_indicators_for_llm_trader)
+from src.utils.utils import calculate_indicators_for_nn
 
 
-def transform_candle_stats(df: pd.DataFrame) -> pd.DataFrame:
+def transform_candle_stats(df: pd.DataFrame, group_by: str) -> pd.DataFrame:
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    # Basic date-time components
     df['day_num'] = df['datetime'].dt.day
-    df['date'] = df['datetime'].dt.date
-    df['date'] = df['date'].apply(
-        lambda x: datetime.strftime(x, "%Y%m%d")
-    ).astype(int)
-
     df['day_of_week'] = df['datetime'].dt.dayofweek
-    df['week_num'] = df['datetime'].dt.isocalendar().week
-    df['year_num'] = df['datetime'].dt.isocalendar().year
-    df['year_day'] = (df['year_num'].astype(str) +
-                      df['day_num'].astype(str))
-    df['year_week'] = (df['year_num'].astype(str) +
-                       df['week_num'].astype(str))
+    df['month'] = df['datetime'].dt.month
+    df['week_of_year'] = df['datetime'].dt.isocalendar().week
 
-    df = calculate_indicators_for_llm_trader(df)
+    # Seasonal transformations
+    df['sin_day_of_year'] = np.sin(2 * np.pi * df['datetime'].dt.dayofyear / 365)
+    df['cos_day_of_year'] = np.cos(2 * np.pi * df['datetime'].dt.dayofyear / 365)
+
+    # Dynamic grouping based on input
+    if group_by == 'day':
+        df['group'] = df['datetime'].dt.strftime('%Y%m%d').astype(int)
+    elif group_by == 'yearweek':
+        df['group'] = df['datetime'].dt.strftime('%G%V')
+    elif group_by == 'month':
+        df['group'] = df['datetime'].dt.strftime('%Y%m')
+    else:
+        raise ValueError(f"Unsupported group_by value: {group_by}")
+
+    df = calculate_indicators_for_nn(df)
     return df
 
 
-def prepare_training_data(exchange):
-    ohlc = exchange.fetch_ohlc(days=365, timeframe='1h')
-    ohcl_df = transform_candle_stats(ohlc)
+def prepare_training_data(exchange, days: int, timeframe: str, group_by: str):
+    ohlc = exchange.fetch_ohlc(days=days, timeframe=timeframe)
+    ohcl_df = transform_candle_stats(ohlc, group_by)
 
     scaler = MinMaxScaler()
     ohcl_df.dropna(inplace=True)
 
-    # Storing original data
-    original = ohcl_df[['timeframe', 'C', 'date', 'year_week', 'atr_20']]
-    original_groups = original.groupby('date')
-    groups_len = set(len(group) for name, group in original_groups)
+    original = ohcl_df[['timeframe', 'C', 'H', 'L', 'group', 'atr_20']]
+    original_groups = original.groupby('group')
+
+    groups_len = set(len(group) for _, group in original_groups)
     groups_max_len = max(groups_len)
-    original_groups = [(name, group) for name, group in original_groups if not len(group) < groups_max_len]
+    original_groups = [(name, group) for name, group in original_groups if len(group) == groups_max_len]
 
-    # Dropping columns not needed for scaling
-    transformed_data_for_scaling = ohcl_df.drop(columns=['year_week', 'year_day', 'datetime'], axis=1)
+    columns_to_scale = ohcl_df.drop(columns=['group', 'datetime'], axis=1).columns
+    scaled_data_array = scaler.fit_transform(ohcl_df[columns_to_scale])
+    joblib.dump(scaler, f'mm_scaler_{timeframe}.gz')
 
-    # Scaling the data
-    scaled_data_array = scaler.fit_transform(transformed_data_for_scaling)
-
-    # Saving the scaler
-    joblib.dump(scaler, 'mm_scaler.gz')
-
-    # Converting scaled data back to DataFrame
-    scaled_data = pd.DataFrame(scaled_data_array, columns=transformed_data_for_scaling.columns,
-                               index=transformed_data_for_scaling.index)
-    scaled_groups = scaled_data.groupby('date')
-    scaled_groups = [(name, group) for name, group in scaled_groups if not len(group) < groups_max_len]
+    scaled_data = pd.DataFrame(scaled_data_array, columns=columns_to_scale, index=ohcl_df.index)
+    scaled_groups = scaled_data.groupby(ohcl_df['group'])
+    scaled_groups = [(name, group) for name, group in scaled_groups if len(group) == groups_max_len]
     zipped = zip(scaled_groups, original_groups)
 
-    return zipped, len(list(scaled_data.columns)), len(original_groups)
+    return zipped, len(columns_to_scale), len(original_groups)
